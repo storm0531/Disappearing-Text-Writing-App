8ac6a388478c0400d5baef28d804a798bca5bbb9
storm0531 <116273220+storm0531@users.noreply.github.com>
2023-01-06 12:48:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+#constants
 BG_COLOR = "skyblue"
 BG_LOSE_COLOR = "#9A1663"
 timer_sec = 60
@@ -11,6 +12,7 @@ stop_timer = False
 
 
 def start_game():
+    #reset game for reuse
     global timer_sec,timer_min,stop_timer,past_word_count
     timer_sec = 60
     timer_min = 4
@@ -19,8 +21,7 @@ def start_game():
 
     start_label.config(text="click to end")
     start_button.config(text="end game",command=end_game)
-
-    text_box.delete("1.0", END)
+    
     text_box.config(state="normal",fg="black")
     text_box.delete("1.0",END)
     text_box.focus()
@@ -30,20 +31,31 @@ def start_game():
 
 def game_running():
     global continue_id,stop_timer
+    
     check_typing()
     run_timer()
+    
+    #if user reaches 5min count down without stoping over 4 seconds user win
     if timer_min == 0 and timer_sec == 0:
         stop_timer = True
         end_game()
-        end_popup(font_color="#FED049",bg_color="#68B984",title="you win",end_results=f"you win with {past_word_count} words wrote \n"
+        end_popup(font_color="#FED049",
+                  bg_color="#68B984",
+                  title="you win",
+                  end_results=f"you win with {past_word_count} words wrote \n"
                   f",and for finishing continuously \n"
                   f"write for 5 Minutes,congratulations")
+        
+    #continue tile either user win the contest or lose the contest
     if not stop_timer:
         continue_id = window.after(1000, game_running)
     else:
         window.after_cancel(continue_id)
+        
 def run_timer():
     global timer_sec, timer_min
+    
+    # reduce 1 second from time remaining
     timer_sec -= 1
     if timer_sec < 10:
         sec = f"0{timer_sec}"
@@ -56,8 +68,14 @@ def run_timer():
     timer.config(text=f"{timer_min}:{sec}")
 
 def check_typing():
+    #check if user is still typing and if stoped it will indicate by color of window
+    #and if stoped typing over 5 sec then it will end the game
     global past_word_count,color_thickness
+    
+    #get length of all words writen by user
     new_word_count = len(text_box.get("1.0",END))
+    
+    #if user stop typing 
     if new_word_count == past_word_count:
         color_thickness += 1
         if color_thickness == 1:
@@ -71,10 +89,14 @@ def check_typing():
         else:
             end_game()
             remaining_time = f"{4 - timer_min} min ,{60 - timer_sec} sec"
-            end_popup(font_color="#EEEEEE",bg_color="#EB455F",title="yoy lost", end_results=f"you lost with {past_word_count} words wrote \n"
+            end_popup(font_color="#EEEEEE",
+                      bg_color="#EB455F",
+                      title="yoy lost", 
+                      end_results=f"you lost with {past_word_count} words wrote \n"
                                   f",and for not typing continued at\n"
                                   f"{remaining_time} elapsed time")
     else:
+        #user still typing or continue typing
         color_thickness = 0
         set_typing()
         past_word_count = new_word_count
@@ -107,7 +129,7 @@ def end_popup(font_color,bg_color,title,end_results):
     results = Label(popup,text=end_results, font=('arial 18 bold'),bg=bg_color,fg=font_color)
     results.grid(column=0,row=0,columnspan=1,rowspan=1)
 
-
+# ------------------- graphical user interface(GUI) -----------------# 
 window = Tk()
 window.title("most dangerous typing game")
 window.minsize(600,400)
